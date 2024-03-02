d4fea8a83eaa4fd763a64374ba81e184e1e7acce
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-03-02 12:59:50 -0300

    feat: creating tests for fatura state

--- gerador-boletos/tests/ProcessadorBoletoTests.java
+++ gerador-boletos/tests/ProcessadorBoletoTests.java
@@ -55,4 +55,36 @@ public class ProcessadorBoletoTests {
         assertEquals(pagamentos, fatura.getPagamentos());
         assertEquals(3, fatura.getPagamentos().size());
     }
+
+    @Test
+    @DisplayName("Testa se o estado da fatura esta sendo trocado ao efetuar um pagamento maior")
+    void testPagamentosAcimaValorFatura(){
+        ProcessadorBoletos pb = new ProcessadorBoletos();
+        Fatura fatura = new Fatura("Gabriel", new Date(), 500);
+
+        ArrayList<Boleto> boletos = new ArrayList<Boleto>();
+        boletos.add(new Boleto("1234", new Date(), 100));
+        boletos.add(new Boleto("4321", new Date(), 250));
+        boletos.add(new Boleto("1324", new Date(), 150));
+
+        List<Pagamento> pagamentos = pb.processa(fatura, boletos);
+
+        assertEquals("PAGA", fatura.getEstado());
+    }
+
+    @Test
+    @DisplayName("Testa se o estado da fatura se mantem o mesmo ao efetuar um pagamento menor")
+    void testPagamentosAbaixoValorFatura(){
+        ProcessadorBoletos pb = new ProcessadorBoletos();
+        Fatura fatura = new Fatura("Gabriel", new Date(), 500);
+
+        ArrayList<Boleto> boletos = new ArrayList<Boleto>();
+        boletos.add(new Boleto("1234", new Date(), 100));
+        boletos.add(new Boleto("4321", new Date(), 50));
+        boletos.add(new Boleto("1324", new Date(), 150));
+
+        List<Pagamento> pagamentos = pb.processa(fatura, boletos);
+
+        assertEquals("PENDENTE", fatura.getEstado());
+    }
 }
