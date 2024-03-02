d6b66033fd4ca53d92c8bfb224a24c2b77cdadc6
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-03-02 11:53:54 -0300

    feat: creating tests for Boleto constructor

---
+++ gerador-boletos/tests/BoletoTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import src.Boleto;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoletoTests {
+    @Test
+    void testCriacaoBoleto() {
+        Date dataCriado = new Date();
+
+        Boleto boleto = new Boleto("12345", dataCriado, 120.0);
+
+        assertEquals("12345", boleto.getCodigo());
+        assertEquals(dataCriado, boleto.getData());
+        assertEquals(120.0, boleto.getValor());
+    }
+}
