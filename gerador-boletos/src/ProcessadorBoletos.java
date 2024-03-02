7125fe6a4448cc7324da99208ec6322dab615505
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-03-02 12:35:57 -0300

    feat: implementing procesa

--- gerador-boletos/src/ProcessadorBoletos.java
+++ gerador-boletos/src/ProcessadorBoletos.java
@@ -1,7 +1,23 @@
 package src;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProcessadorBoletos {
     public ProcessadorBoletos() {
 
+    }
+
+    public List<Pagamento> processa(Fatura fatura, List<Boleto> boletos) {
+        List<Pagamento> pagamentosEfetuados = new ArrayList<>();
+
+        for (Boleto boleto : boletos) {
+            Pagamento novoPagamento = new Pagamento(boleto.getValor(), boleto.getData(), "BOLETO");
+
+            pagamentosEfetuados.add(novoPagamento);
+        }
+
+        return pagamentosEfetuados;
     }
 }
