c551f69913cfca990048e1ad8d4f44bebd1b9d73
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-03-02 12:28:02 -0300

    feat: creating pagamento class

---
+++ gerador-boletos/src/Pagamento.java
@@ -0,0 +1,27 @@
+package src;
+
+import java.util.Date;
+
+public class Pagamento {
+    private double valor;
+    private Date data;
+    private String tipo;
+
+    public Pagamento(double valor, Date data, String tipo) {
+        this.valor = valor;
+        this.data = data;
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return this.valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
